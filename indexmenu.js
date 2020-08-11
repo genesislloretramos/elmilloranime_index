5fd8c0aafad310d1e7ec87ecacbca9d6e14e10d3
GÉNESIS LLORET RAMOS <56254236+genesislloretramos@users.noreply.github.com>
2020-08-11 13:55:05 +0200

    Create indexmenu.js

---
+++ indexmenu.js
@@ -0,0 +1,152 @@
+  function clear() {
+  	document.getElementById("ALTitle").innerHTML = "";
+    document.getElementById("clear").innerHTML = "";
+  	document.getElementById("ALResult").innerHTML = "";
+  };
+  
+  function load_default(){
+  	clear();
+    document.getElementById("AlResult").innerHTML = "<div class='target'><a href='"+""+"'><div class='img-thumbnail2'><span class='overlay2'>::before<div class='info-wrapper2'>Veure Online!</div>::after</span><img src='' alt='"+""+"' title='"+""+"'></div></a><span class='label-info2'><i class='fa fa-tag'></i>"+"PEL·LÍCULES"+"</span><h2 class='post-title2 entry-title2'><a href='"+""+"' title='"+""+"'>"+""+"</a></h2><div class='aishare'><ul><li><a class='lyco-twitter' href='http://twitter.com/share?url="+""+"' rel='nofollow' target='_blank'><i class='fa fa-twitter'></i><span class='inv'></span></a></li><li><a class='lyco-facebook' href='http://www.facebook.com/sharer.php?u="+""+"' rel='nofollow' target='_blank'><i class='fa fa-facebook'></i><span class='inv'></span></a></li></ul></div><div class='target'><a href='"+""+"'></div></div>";
+  };
+  
+  function clear() {
+  	document.getElementById("ALTitle").innerHTML = "";
+    document.getElementById("clear").innerHTML = "";
+  	document.getElementById("ALResult").innerHTML = "";
+  };
+  
+  function load_a() {
+    clear();
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra A";
+  	document.getElementById("ALResult").innerHTML = " "+
+      "<!-- Akira -->"+
+      "<div class='target'><a href='https://elmilloranime.blogspot.com/1988/01/akira.html'><div class='img-thumbnail2'><span class='overlay2'>::before<div class='info-wrapper2'>Veure Online!</div>::after</span><img src='https://4.bp.blogspot.com/-JFDJJS_IhA4/XS_FGmLpA1I/AAAAAAAADCY/PYI9b_gn3hoUPO66TaVRbLP0YCkxvIuzwCLcBGAs/w0-h0-c/Akira_1988_poster.jpg' alt='Akira' title='Akira'></div></a><span class='label-info2'><i class='fa fa-tag'></i>PEL·LÍCULES</span><h2 class='post-title2 entry-title2'><a href='https://elmilloranime.blogspot.com/1988/01/akira.html' title='Akira'>AKIRA</a></h2><div class='aishare'><ul><li><a class='lyco-twitter' href='http://twitter.com/share?url=https://elmilloranime.blogspot.com/1988/01/akira.html' rel='nofollow' target='_blank'><i class='fa fa-twitter'></i><span class='inv'></span></a></li><li><a class='lyco-facebook' href='http://www.facebook.com/sharer.php?u=https://elmilloranime.blogspot.com/1988/01/akira.html' rel='nofollow' target='_blank'><i class='fa fa-facebook'></i><span class='inv'></span></a></li></ul></div><div class='target'><a href='https://elmilloranime.blogspot.com/1988/01/akira.html'></div></div>"+
+	  " "+
+      "<!-- Alita: Angel de combat -->"+
+      "<div class='target'><a href='https://elmilloranime.blogspot.com/1993/01/alita-angel-de-combat.html'><div class='img-thumbnail2'><span class='overlay2'>::before<div class='info-wrapper2'>Veure Online!</div>::after</span><img src='https://1.bp.blogspot.com/-hWl4ojxtDvE/XTDN-lFOurI/AAAAAAAADDk/zqDW2ywZdAEVQeioIpQOVDz59YSLPRWkACLcBGAs/s1600/cover.alita' alt='ALITA: ANGEL DE COMBAT' title='ALITA: ANGEL DE COMBAT'></div></a><span class='label-info2'><i class='fa fa-tag'></i>"+"OVAS"+"</span><h2 class='post-title2 entry-title2'><a href='https://elmilloranime.blogspot.com/1993/01/alita-angel-de-combat.html' title='ALITA: ANGEL DE COMBAT'>"+"ALITA: ANGEL DE COMBAT"+"</a></h2><div class='aishare'><ul><li><a class='lyco-twitter' href='http://twitter.com/share?url='https://elmilloranime.blogspot.com/1993/01/alita-angel-de-combat.html' rel='nofollow' target='_blank'><i class='fa fa-twitter'></i><span class='inv'></span></a></li><li><a class='lyco-facebook' href='http://www.facebook.com/sharer.php?u=https://elmilloranime.blogspot.com/1993/01/alita-angel-de-combat.html' rel='nofollow' target='_blank'><i class='fa fa-facebook'></i><span class='inv'></span></a></li></ul></div><div class='target'><a href='https://elmilloranime.blogspot.com/1993/01/alita-angel-de-combat.html'></div></div>"+
+      " "+
+      "<!-- Amor a Tokyo -->"+
+      "<div class='target'><a href='https://elmilloranime.blogspot.com/2007/11/amor-tokyo.html'> <div class='img-thumbnail2'><span class='overlay2'>::before<div class='info-wrapper2'>Veure Online!</div>::after</span><img src='https://2.bp.blogspot.com/-lJTnlYRHVqI/XUG-fiVIMJI/AAAAAAAADNU/WfSR4AFqrjorFEU_io6-9nqaEA6SItQtQCLcBGAs/s1600/Tokyo%2BMarble%2BChocolate.jpg' alt='AMOR A TOKYO' title='AMOR A TOKYO'></div></a><span class='label-info2'><i class='fa fa-tag'></i>PEL·LÍCULES</span> <h2 class='post-title2 entry-title2'><a href='https://elmilloranime.blogspot.com/2007/11/amor-tokyo.html' title='AMOR A TOKYO'>AMOR A TOKYO</a></h2> <div class='aishare'> <ul> <li><a class='lyco-twitter' href='http://twitter.com/share?url=https://elmilloranime.blogspot.com/2007/11/amor-tokyo.html' rel='nofollow' target='_blank'><i class='fa fa-twitter'></i><span class='inv'></span></a></li><li><a class='lyco-facebook' href='http://www.facebook.com/sharer.php?u=https://elmilloranime.blogspot.com/2007/11/amor-tokyo.html' rel='nofollow' target='_blank'><i class='fa fa-facebook'></i><span class='inv'></span></a></li></ul> </div><div class='target'><a href='https://elmilloranime.blogspot.com/2007/11/amor-tokyo.html'></div></div>"+
+      " "+
+      "<!-- Ancien i el món-màgic -->"+
+      "<div class='target'><a href='https://elmilloranime.blogspot.com/2017/03/ancien-i-el-mon-magic.html'> <div class='img-thumbnail2'><span class='overlay2'>::before<div class='info-wrapper2'>Veure Online!</div>::after</span><img src='https://3.bp.blogspot.com/-MCucBpy-oG8/XUO53Yh7WfI/AAAAAAAADO8/5sD3vDpqQkcGkMgWFLJm9nov8OXHPKdVgCLcBGAs/s1600/poster_0_3_900x0.jpg' alt='ANCIEN I EL MÓN MÀGIC' title='ANCIEN I EL MÓN MÀGIC'></div></a><span class='label-info2'><i class='fa fa-tag'></i>PEL·LÍCULES</span> <h2 class='post-title2 entry-title2'><a href='https://elmilloranime.blogspot.com/2017/03/ancien-i-el-mon-magic.html' title='ANCIEN I EL MÓN MÀGIC'>ANCIEN I EL MÓN MÀGIC</a></h2> <div class='aishare'> <ul> <li><a class='lyco-twitter' href='http://twitter.com/share?url=https://elmilloranime.blogspot.com/2017/03/ancien-i-el-mon-magic.html' rel='nofollow' target='_blank'><i class='fa fa-twitter'></i><span class='inv'></span></a></li><li><a class='lyco-facebook' href='http://www.facebook.com/sharer.php?u=https://elmilloranime.blogspot.com/2017/03/ancien-i-el-mon-magic.html' rel='nofollow' target='_blank'><i class='fa fa-facebook'></i><span class='inv'></span></a></li></ul> </div><div class='target'><a href='https://elmilloranime.blogspot.com/2017/03/ancien-i-el-mon-magic.html'></div></div>"+
+      " "+
+      "<!-- Appleseed -->"+
+      "<div class='target'><a href='https://elmilloranime.blogspot.com/1988/11/appleseed.html'> <div class='img-thumbnail2'><span class='overlay2'>::before<div class='info-wrapper2'>Veure Online!</div>::after</span><img src='https://4.bp.blogspot.com/-0OXb7Dx6uRg/XT8yblLl51I/AAAAAAAADI4/hsWMJaV_lSwy78AoX0umGe-4iXCUYNP6ACPcBGAYYCw/s1600/appleseed1988.jpg' alt='APPLESEED' title='APPLESEED'></div></a><span class='label-info2'><i class='fa fa-tag'></i>OVAS</span> <h2 class='post-title2 entry-title2'><a href='https://elmilloranime.blogspot.com/1988/11/appleseed.html' title='APPLESEED'>APPLESEED</a></h2> <div class='aishare'> <ul> <li><a class='lyco-twitter' href='http://twitter.com/share?url=https://elmilloranime.blogspot.com/1988/11/appleseed.html' rel='nofollow' target='_blank'><i class='fa fa-twitter'></i><span class='inv'></span></a></li><li><a class='lyco-facebook' href='http://www.facebook.com/sharer.php?u=https://elmilloranime.blogspot.com/1988/11/appleseed.html' rel='nofollow' target='_blank'><i class='fa fa-facebook'></i><span class='inv'></span></a></li></ul> </div><div class='target'><a href='https://elmilloranime.blogspot.com/1988/11/appleseed.html'></div></div>"+
+      ""+
+      "<!-- Arc the Lad -->"+
+      "<div class='target'><a href='https://elmilloranime.blogspot.com/1999/10/arc-lad.html'> <div class='img-thumbnail2'><span class='overlay2'>::before<div class='info-wrapper2'>Veure Online!</div>::after</span><img src='https://2.bp.blogspot.com/-rcnZgLdAwfc/XS_Fo4zYNeI/AAAAAAAADCg/CmaPwEwLL-oEcpBP5qczlwarc7XTQpkJACLcBGAs/s1600/51Y51MXAQ3L.jpg' alt='ARC THE LAD' title='ARC THE LAD'></div></a><span class='label-info2'><i class='fa fa-tag'></i>OVAS</span> <h2 class='post-title2 entry-title2'><a href='https://elmilloranime.blogspot.com/1999/10/arc-lad.html' title='ARC THE LAD'>ARC THE LAD</a></h2> <div class='aishare'> <ul> <li><a class='lyco-twitter' href='http://twitter.com/share?url=https://elmilloranime.blogspot.com/1999/10/arc-lad.html' rel='nofollow' target='_blank'><i class='fa fa-twitter'></i><span class='inv'></span></a></li><li><a class='lyco-facebook' href='http://www.facebook.com/sharer.php?u=https://elmilloranime.blogspot.com/1999/10/arc-lad.html' rel='nofollow' target='_blank'><i class='fa fa-facebook'></i><span class='inv'></span></a></li></ul> </div><div class='target'><a href='https://elmilloranime.blogspot.com/1999/10/arc-lad.html'></div></div>"+
+      " "+
+      "<!-- Arió -->"+
+	  "<div class='target'><a href='https://elmilloranime.blogspot.com/1986/02/ario.html'> <div class='img-thumbnail2'><span class='overlay2'>::before<div class='info-wrapper2'>Veure Online!</div>::after</span><img src='https://2.bp.blogspot.com/-6KqmJGcn2XM/XUG9ywN5R2I/AAAAAAAADNM/UdeLTTAG080fySILaeXwMAqDcFUlHnkHQCLcBGAs/s1600/Arion.jpg' alt='ARIÓ' title='ARIÓ'></div></a><span class='label-info2'><i class='fa fa-tag'></i>PEL·LÍCULES</span> <h2 class='post-title2 entry-title2'><a href='https://elmilloranime.blogspot.com/1986/02/ario.html' title='ARIÓ'>ARIÓ</a></h2> <div class='aishare'> <ul> <li><a class='lyco-twitter' href='http://twitter.com/share?url=https://elmilloranime.blogspot.com/1986/02/ario.html' rel='nofollow' target='_blank'><i class='fa fa-twitter'></i><span class='inv'></span></a></li><li><a class='lyco-facebook' href='http://www.facebook.com/sharer.php?u=https://elmilloranime.blogspot.com/1986/02/ario.html' rel='nofollow' target='_blank'><i class='fa fa-facebook'></i><span class='inv'></span></a></li></ul> </div><div class='target'><a href='https://elmilloranime.blogspot.com/1986/02/ario.html'></div></div>"+
+      ""+
+      "<!-- ARRIETTY I EL MON DELS REMENUTS -->"+
+      "<div class='target'><a href='https://elmilloranime.blogspot.com/2010/07/arrietty-i-el-mon-dels-remenuts.html'> <div class='img-thumbnail2'><span class='overlay2'>::before<div class='info-wrapper2'>Veure Online!</div>::after</span><img src='https://2.bp.blogspot.com/-98m_SjUhNzI/XTbFOg73iDI/AAAAAAAADFE/qi6Dvbi3FtQuuV-iKeSTElLS3NuzmPsFACLcBGAs/s1600/Arrietty%2Bi%2Bel%2BMon%2Bdels%2BRemenuts.jpg' alt='ARRIETTY I EL MON DELS REMENUTS' title='ARRIETTY I EL MON DELS REMENUTS'></div></a><span class='label-info2'><i class='fa fa-tag'></i>PEL·LÍCULES</span> <h2 class='post-title2 entry-title2'><a href='https://elmilloranime.blogspot.com/2010/07/arrietty-i-el-mon-dels-remenuts.html' title='ARRIETTY I EL MON DELS REMENUTS'>ARRIETTY I EL MON DELS REMENUTS</a></h2> <div class='aishare'> <ul> <li><a class='lyco-twitter' href='http://twitter.com/share?url=https://elmilloranime.blogspot.com/2010/07/arrietty-i-el-mon-dels-remenuts.html' rel='nofollow' target='_blank'><i class='fa fa-twitter'></i><span class='inv'></span></a></li><li><a class='lyco-facebook' href='http://www.facebook.com/sharer.php?u=https://elmilloranime.blogspot.com/2010/07/arrietty-i-el-mon-dels-remenuts.html' rel='nofollow' target='_blank'><i class='fa fa-facebook'></i><span class='inv'></span></a></li></ul> </div><div class='target'><a href='https://elmilloranime.blogspot.com/2010/07/arrietty-i-el-mon-dels-remenuts.html'></div></div>"+
+      " "+
+      "<!-- ASTRO BOY -->"+
+      "<div class='target'><a href='https://elmilloranime.blogspot.com/2008/01/astro-boy.html'> <div class='img-thumbnail2'><span class='overlay2'>::before<div class='info-wrapper2'>Veure Online!</div>::after</span><img src='https://4.bp.blogspot.com/-SRIQsHsTh7k/XTI0x3_VF0I/AAAAAAAADEI/qMHVYWxMYGkMzl1nTFaz6LvrxiRz3IP0ACLcBGAs/s1600/astro_boy_astroboy-600990296-large.jpg' alt='ASTRO BOY' title='ASTRO BOY'></div></a><span class='label-info2'><i class='fa fa-tag'></i>PEL·LÍCULES</span> <h2 class='post-title2 entry-title2'><a href='https://elmilloranime.blogspot.com/2008/01/astro-boy.html' title='ASTRO BOY'>ASTRO BOY</a></h2> <div class='aishare'> <ul> <li><a class='lyco-twitter' href='http://twitter.com/share?url=https://elmilloranime.blogspot.com/2008/01/astro-boy.html' rel='nofollow' target='_blank'><i class='fa fa-twitter'></i><span class='inv'></span></a></li><li><a class='lyco-facebook' href='http://www.facebook.com/sharer.php?u=https://elmilloranime.blogspot.com/2008/01/astro-boy.html' rel='nofollow' target='_blank'><i class='fa fa-facebook'></i><span class='inv'></span></a></li></ul> </div><div class='target'><a href='https://elmilloranime.blogspot.com/2008/01/astro-boy.html'></div></div>";
+  };
+  
+  function load_b() {
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra B";
+  };
+  
+  function load_c() {
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra C";
+  };
+  
+  function load_d() {
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra D";
+  };
+  
+  function load_e() {
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra E";
+  };
+  
+  function load_f() {
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra F";
+  };
+  
+  function load_g(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra G";
+  };
+  
+  function load_h(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra H";
+  };
+  
+  function load_i(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra I";
+  };
+  
+  function load_j(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra J";
+  };
+  
+  function load_k(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra Q";
+  };
+  
+  function load_l(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra L";
+  };
+  function load_m(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra M";
+  };
+  
+  function load_n(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra N";
+  };
+  
+  function load_o(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra O";
+  };
+  
+  function load_p(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra P";
+  };
+  
+  function load_c(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra Q";
+  };
+  
+  function load_r(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra R";
+  };
+  
+  function load_s(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra S";
+  };
+  
+  function load_t(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra T";
+  };
+  
+  function load_u(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra U";
+  };
+  
+  function load_v(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra V";
+  };
+  
+  function load_w(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra W";
+  };
+  
+  function load_x(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra X";
+  };
+  
+  function load_y(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra Y";
+  };
+  
+  function load_z(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb la lletra Z";
+  };
+  
+  function load_#(){
+    document.getElementById("ALTitle").innerHTML = "Animes amb Caràcters Especials";
+  };
+  
